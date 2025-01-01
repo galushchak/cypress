10684d1e92bb5adbbb772acc2dc81036962f239c
galushchak <galushchak@inbox.lv>
2025-01-01 18:38:32 +0200

    Minor changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress';
 // @ts-ignore
 import { allureCypress } from 'allure-cypress/reporter';
+import { Status } from 'allure-js-commons';
+import * as os from 'node:os';
 
 export default defineConfig({
     e2e: {
@@ -32,6 +34,33 @@ export default defineConfig({
             // noinspection Annotator
             allureCypress(on, config, {
                 resultsDir: 'result/allure-results',
+                links: {
+                    issue: {
+                        nameTemplate: 'Issue #%s',
+                        urlTemplate: 'https://issues.example.com/%s',
+                    },
+                    tms: {
+                        nameTemplate: 'TMS #%s',
+                        urlTemplate: 'https://tms.example.com/%s',
+                    },
+                    jira: {
+                        urlTemplate: v => `https://jira.example.com/browse/${v}`,
+                    },
+                },
+                categories: [
+                    {
+                        name: 'foo',
+                        messageRegex: 'bar',
+                        traceRegex: 'baz',
+                        matchedStatuses: [Status.FAILED, Status.BROKEN],
+                    },
+                ],
+                environmentInfo: {
+                    os_platform: os.platform(),
+                    os_release: os.release(),
+                    os_version: os.version(),
+                    node_version: process.version,
+                },
             });
             return config;
         },
--- cypress.test.ts
+++ cypress.test.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress';
 import { allureCypress } from 'allure-cypress/reporter';
+import { Status } from 'allure-js-commons';
+import os from 'node:os';
 
 export default defineConfig({
     e2e: {
@@ -31,6 +33,33 @@ export default defineConfig({
             // noinspection Annotator
             allureCypress(on, config, {
                 resultsDir: 'result/allure-results',
+                links: {
+                    issue: {
+                        nameTemplate: 'Issue #%s',
+                        urlTemplate: 'https://issues.example.com/%s',
+                    },
+                    tms: {
+                        nameTemplate: 'TMS #%s',
+                        urlTemplate: 'https://tms.example.com/%s',
+                    },
+                    jira: {
+                        urlTemplate: v => `https://jira.example.com/browse/${v}`,
+                    },
+                },
+                categories: [
+                    {
+                        name: 'foo',
+                        messageRegex: 'bar',
+                        traceRegex: 'baz',
+                        matchedStatuses: [Status.FAILED, Status.BROKEN],
+                    },
+                ],
+                environmentInfo: {
+                    os_platform: os.platform(),
+                    os_release: os.release(),
+                    os_version: os.version(),
+                    node_version: process.version,
+                },
             });
             return config;
         },
