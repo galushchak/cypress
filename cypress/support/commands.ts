b70e828039a47ef3c951b5e276323a7e5237efcb
galushchak <galushchak@inbox.lv>
2025-01-04 09:07:58 +0200

    Refactor accessibility checks to use a dedicated highlight function for improved clarity and maintainability

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 import { Result } from 'axe-core';
+import { highlightElements } from './utils/highlightElements';
 
 interface Violation {
     id: string;
@@ -11,15 +12,15 @@ interface Violation {
 
 Cypress.Commands.add('runAccessibilityChecks', () => {
     cy.injectAxe();
-    cy.checkA11y(undefined, undefined, (violations: Result[]) => {
-        let violationObj: Violation[] = new Array<Violation>();
+    cy.checkA11y(undefined, undefined, (results: Result[]) => {
+        let violations: Violation[] = new Array<Violation>();
 
         cy.task('log', 'Accessibility violation(-s):', { log: false });
-        violations.forEach(violation => {
+        results.forEach(violation => {
             cy.log(
                 `${violation.id} - ${violation.impact.toUpperCase()}: ${violation.nodes.map(node => node.target.toString()).join(';')}`,
             );
-            violationObj.push({
+            violations.push({
                 id: violation.id,
                 impact: violation.impact,
                 description: violation.description,
@@ -29,44 +30,8 @@ Cypress.Commands.add('runAccessibilityChecks', () => {
             });
         });
 
-        violationObj.forEach(violation => {
-            cy.window({ log: false }).scrollTo(0, 0, { log: false });
-            const bodyRect = Cypress.$('body')[0].getBoundingClientRect();
+        highlightElements(violations);
 
-            violation.elements
-                .map(element => {
-                    const el = Cypress.$(element)[0];
-                    const rect = el != null ? el.getBoundingClientRect() : { x: 0, y: 0, width: 0, height: 0 };
-                    return {
-                        selector: element,
-                        x: Math.max(rect.x - bodyRect.x, 0),
-                        y: Math.max(rect.y - bodyRect.y, 0),
-                        width: Math.max(rect.width, 0),
-                        height: Math.max(rect.height, 0),
-                    };
-                })
-                .filter(element => element.x > 0 && element.y > 0)
-                .forEach(element => {
-                    let highlightElement = document.createElement('div');
-                    const highlightElementStyle = {
-                        backgroundColor: 'rgba(255, 0, 0, 0.2)',
-                        borderRadius: '5px',
-                        boxSizing: 'border-box',
-                        height: `${element.height}px`,
-                        left: `${element.x}px`,
-                        outline: '1px solid rgba(255, 0, 0, 0.2)',
-                        pointerEvents: 'none',
-                        position: 'absolute',
-                        top: `${element.y}px`,
-                        width: `${element.width}px`,
-                        zIndex: '9999',
-                    };
-
-                    Object.assign(highlightElement.style, highlightElementStyle);
-                    Cypress.$('body').append(highlightElement);
-                });
-        });
-
-        cy.task('log', JSON.parse(JSON.stringify(violationObj, null, 2)), { log: false });
+        cy.task('log', JSON.parse(JSON.stringify(violations, null, 2)), { log: false });
     });
 });
---
+++ cypress/support/utils/highlightElements.ts
@@ -0,0 +1,40 @@
+export function highlightElements(violations: any[]): void {
+    violations.forEach((violation: { elements: any[] }) => {
+        cy.window({ log: false }).scrollTo(0, 0, { log: false });
+
+        const bodyRect = Cypress.$('body')[0].getBoundingClientRect();
+
+        violation.elements
+            .map((element: any) => {
+                const el = Cypress.$(element)[0];
+                const rect = el != null ? el.getBoundingClientRect() : { x: 0, y: 0, width: 0, height: 0 };
+                return {
+                    selector: element,
+                    x: Math.max(rect.x - bodyRect.x, 0),
+                    y: Math.max(rect.y - bodyRect.y, 0),
+                    width: Math.max(rect.width, 0),
+                    height: Math.max(rect.height, 0),
+                };
+            })
+            .filter((element: { x: number; y: number }) => element.x > 0 && element.y > 0)
+            .forEach((element: { height: any; x: any; y: any; width: any }) => {
+                let highlightElement = document.createElement('div');
+                const highlightElementStyle = {
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)',
+                    borderRadius: '5px',
+                    boxSizing: 'border-box',
+                    height: `${element.height}px`,
+                    left: `${element.x}px`,
+                    outline: '1px solid rgba(255, 0, 0, 0.2)',
+                    pointerEvents: 'none',
+                    position: 'absolute',
+                    top: `${element.y}px`,
+                    width: `${element.width}px`,
+                    zIndex: '9999',
+                };
+
+                Object.assign(highlightElement.style, highlightElementStyle);
+                Cypress.$('body').append(highlightElement);
+            });
+    });
+}
